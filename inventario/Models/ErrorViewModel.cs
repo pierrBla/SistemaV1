7ea76a01ccee8c588a6369fda7aacea39b29b7b1
pierrBla <pierrcalderon6@hotmail.com>
2023-11-28 16:16:11 -0500

    mi primer proyecto

---
+++ inventario/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace inventario.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
